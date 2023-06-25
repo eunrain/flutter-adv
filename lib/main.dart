8cf50d807750aac921354da4c9d7b4b3462e850d
goeunrain <boniteterain@gmail.com>
2023-06-25 22:28:33 +0900

    feat : rendering content conditionally

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sec_app/start_screen.dart';
+import 'package:sec_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 52, 6, 105),
-                Color.fromARGB(255, 107, 67, 133),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:sec_app/questions_screen.dart';
+import 'package:sec_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 52, 6, 105),
+                Color.fromARGB(255, 107, 67, 133),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
