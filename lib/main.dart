015c60ec35df1df106a1287dbcbd1323efd0d1df
goeunrain <boniteterain@gmail.com>
2023-06-25 20:33:56 +0900

    feat : challenge solution - working with more widgets

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sec_app/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 52, 6, 105),
+                Color.fromARGB(255, 107, 67, 133),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
