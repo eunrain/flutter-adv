0460a5b86b921f3421047756213ddc2d50a256bd
goeunrain <boniteterain@gmail.com>
2023-06-26 14:21:03 +0900

    feat : mutating values in memory - shuffle

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,7 +1,13 @@
 class QuizQuestion {
   //위젯이 아니기 때문에 확장할 필요가 없음
-  const QuizQuestion(this.text, this.answer);
+  const QuizQuestion(this.text, this.answers);
 
   final String text;
-  final List<String> answer;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
