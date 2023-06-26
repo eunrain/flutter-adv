0edcc364d4f12f35c177a22106d503f857c8bcd4
goeunrain <boniteterain@gmail.com>
2023-06-26 11:23:27 +0900

    feat : configuring a column with mainAxisAlignment

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,7 @@
+class QuizQuestion {
+  //위젯이 아니기 때문에 확장할 필요가 없음
+  const QuizQuestion(this.text, this.answer);
+
+  final String text;
+  final List<String> answer;
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,30 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    // 중앙 위젯을 래퍼로 사용하고 축 크기를 설정하는 대안 - 중심축 정렬을 정할 수 있다는 장점이 있음
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
